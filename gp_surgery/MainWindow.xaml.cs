7152c0d870f7593fb9a85218933ebf5311cd6325
Brandon Lee <81641705+brandonwklee@users.noreply.github.com>
2021-04-08 18:44:33 +0100

    Started C# project using WPF
    This is first project where a GUI and SQL server is used in an app.

---
+++ gp_surgery/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gp_surgery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["gp_surgery.Properties.Settings.gp_surgeryConnectionString"].ConnectionString;
+        }
+    }
+}
